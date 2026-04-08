33f86cea40a21a1cb9a2e1b2880146a0c2447a22
Olga Lunevich <olga.lunevich@gmail.com>
2026-04-08 18:16:15 +0300

    refactor(TH-8): replace string unions with enums and unify types

--- src/domain/tournaments/contracts/inputs/create-tournament.input.ts
+++ src/domain/tournaments/contracts/inputs/create-tournament.input.ts
@@ -1,7 +1,9 @@
+import type { TournamentVisibility } from '@src/domain/tournaments/enums/tournament-visibility.enum';
+
 export interface CreateTournamentInput {
   title: string;
   description?: string;
-  visibility: 'public' | 'private';
+  visibility: TournamentVisibility;
   roundsCount: number;
   ownerId: string;
 }
