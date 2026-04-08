5e45218a703583e5ba3530c8a1d520d421734a32
Olga Lunevich <olga.lunevich@gmail.com>
2026-04-08 19:19:33 +0300

    refactor(TH-8): expand tournament-participant entity with date columns, improved repository

--- src/domain/tournaments/repositories/tournament-participant.repository.ts
+++ src/domain/tournaments/repositories/tournament-participant.repository.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectEntityManager } from '@nestjs/typeorm';
-import { EntityManager, Repository } from 'typeorm';
+import { EntityManager, type QueryRunner, Repository, type SelectQueryBuilder } from 'typeorm';
 
 import { TournamentParticipantEntity } from '../entities/tournament-participant.entity';
 
@@ -8,5 +8,21 @@ import { TournamentParticipantEntity } from '../entities/tournament-participant.
 export class TournamentParticipantRepository extends Repository<TournamentParticipantEntity> {
   constructor(@InjectEntityManager() entityManager: EntityManager) {
     super(TournamentParticipantEntity, entityManager);
+  }
+
+  protected get tableAlias(): string {
+    return this.metadata.tableName;
+  }
+
+  async getOneByIds(tournamentId: string, userId: string): Promise<TournamentParticipantEntity> {
+    const entity = await this.findOne({ where: { tournamentId, userId } });
+    if (!entity) {
+      throw new NotFoundException(`${this.tableAlias}.main.NOT_FOUND`);
+    }
+    return entity;
+  }
+
+  protected getBaseQuery(queryRunner?: QueryRunner): SelectQueryBuilder<TournamentParticipantEntity> {
+    return this.createQueryBuilder(this.tableAlias, queryRunner);
   }
 }
