5b7c2893e0502d4d4cf4851375ae2aff1d2ee33a
Andrei Kanavalau <timeworstseal@gmail.com>
2026-04-09 20:41:02 +0200

    fix(TH-11): Fixed fingerprint retrieval

--- src/modules/fingerprint/decorators/fingerprint-payload.decorator.ts
+++ src/modules/fingerprint/decorators/fingerprint-payload.decorator.ts
@@ -3,7 +3,7 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { Fingerprint } from '../interfaces/fingerprint.interface';
 
 export const FingerprintPayload = createParamDecorator((_, ctx: ExecutionContext): Fingerprint => {
-  const request: Request & { cookies: { fp: string } } = ctx.switchToHttp().getRequest();
+  const request: Request & { fp: string } = ctx.switchToHttp().getRequest();
 
-  return { id: request.cookies.fp };
+  return { id: request.fp };
 });
