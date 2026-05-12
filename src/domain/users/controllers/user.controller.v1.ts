7543bef200e6d165c14ad6efd6ba001f3bc3d02b
Andrei Kanavalau <timeworstseal@gmail.com>
2026-05-12 21:04:31 +0200

    feat(TH-19): expose live tournament recovery endpoint

--- src/domain/users/controllers/user.controller.v1.ts
+++ src/domain/users/controllers/user.controller.v1.ts
@@ -5,14 +5,19 @@ import { ApiResponse } from '@shared/decorators/api-response.decorator';
 import { UserPayload } from '@shared/decorators/user-payload.decorator';
 import type { JwtUserPayload } from '@shared/interfaces/jwt-user-payload.interface';
 import { AccessTokenGuard } from '@src/domain/auth/guards/access-token.guard';
+import { GetLiveTournamentResponseRTO } from '@src/domain/users/contracts/rto/get-live-tournament-response.rto';
 import { UserProfileRTO } from '@src/domain/users/contracts/rto/user-profile.rto';
+import { GetLiveTournamentQueryService } from '@src/domain/users/services/get-live-tournament-query.service';
 import { UserService } from '@src/domain/users/services/user.service';
 
 @ApiTags('Users')
 @UseGuards(AccessTokenGuard)
 @ApiController('users', 1)
 export class UserControllerV1 {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly getLiveTournamentQueryService: GetLiveTournamentQueryService,
+  ) {}
 
   @ApiOperation({ summary: 'Get profile' })
   @ApiResponse(HttpStatus.OK, UserProfileRTO)
@@ -21,5 +26,12 @@ export class UserControllerV1 {
     const dto = { id: user.id };
 
     return this.userService.getProfile(dto);
+  }
+
+  @ApiOperation({ summary: 'Get current active live tournament' })
+  @ApiResponse(HttpStatus.OK, GetLiveTournamentResponseRTO)
+  @Get('me/live-tournament')
+  async getLiveTournament(@UserPayload() user: JwtUserPayload): Promise<GetLiveTournamentResponseRTO> {
+    return this.getLiveTournamentQueryService.execute(user.id);
   }
 }
