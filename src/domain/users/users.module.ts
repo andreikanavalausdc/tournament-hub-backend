03b77f82bf6861ae554119211d53a94151176a52
Andrei Kanavalau <timeworstseal@gmail.com>
2026-05-12 21:04:31 +0200

    feat(TH-19): expose live tournament recovery endpoint

--- src/domain/users/users.module.ts
+++ src/domain/users/users.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TournamentsModule } from '@src/domain/tournaments/tournaments.module';
 import { UserControllerV1 } from '@src/domain/users/controllers/user.controller.v1';
 import { UserRepository } from '@src/domain/users/repositories/user.repository';
+import { GetLiveTournamentQueryService } from '@src/domain/users/services/get-live-tournament-query.service';
 import { PasswordHashingService } from '@src/domain/users/services/password-hashing.service';
 import { UserService } from '@src/domain/users/services/user.service';
 
 @Module({
-  imports: [],
+  imports: [TournamentsModule],
   controllers: [UserControllerV1],
-  providers: [UserRepository, UserService, PasswordHashingService],
+  providers: [UserRepository, UserService, PasswordHashingService, GetLiveTournamentQueryService],
   exports: [UserService],
 })
 export class UsersModule {}
