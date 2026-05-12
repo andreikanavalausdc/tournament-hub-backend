89da76e0c86a4bdf1b0b038ca8c11c7a9a15bdc3
Andrei Kanavalau <timeworstseal@gmail.com>
2026-05-12 22:58:56 +0200

    feat(TH-21): serve lazy asyncapi docs

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { setAppSettings } from '@shared/helpers/set-app-settings.helper';
+import { setUpAsyncApi } from '@shared/helpers/set-up-asyncapi.helper';
 import { environment } from '@src/environment';
 
 import { AppModule } from './app.module';
@@ -8,6 +9,10 @@ async function bootstrap(): Promise<void> {
   let app = await NestFactory.create(AppModule);
 
   app = await setAppSettings(app, { enableSwagger: true });
+
+  if (environment.app.swaggerEnabled) {
+    setUpAsyncApi(app);
+  }
 
   await app.listen(environment.app.port);
 }
